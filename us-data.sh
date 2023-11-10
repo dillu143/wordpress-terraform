766629f40543dfb0682ca8e019a2674f73e1c4f4
EC2 Default User <ec2-user@ip-10-10-115-69.ec2.internal>
2023-11-10 11:09:23 +0000

    updatiing the code

--- us-data.sh
+++ us-data.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
-sudo apt update -y
-sudo apt upgrade -y
-sudo apt install git -y
-sudo apt install apache2 -y
-cd /var/www/html/
-sudo rm -rf *
+sudo yum update -y
+sudo yum install httpd -y
+sudo systemctl start httpd
+sudo systemctl enable httpd
+sudo yum -y install git
+sudo yum -y install mysql*
+sudo yum install php-gd php-json php-mbstring php-mysqlnd php-opcache php-pdo php-pecl-igbinary -y
+sudo amazon-linux-extras install php8.2 -y
 sudo git clone https://github.com/dillu143/sample.git /var/www/html/
-sudo apt install php -y
-sudo apt install mysql* -y
-sudo systemctl restart apache2
-sudo apt install mariadb* -y
+sudo systemctl restart httpd
